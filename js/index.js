9e4c64cb84b1ef59a2ab8f3c51f35f93305a595b
Katiuska <katiuskaalicea@gmail.com>
2020-07-10 00:05:41 -0700

    created mouseenter event

--- js/index.js
+++ js/index.js
@@ -1,1 +1,24 @@
 // Your code goes here
+
+const h1 = document.querySelector('h1');
+const introImg = document.querySelector('.intro').firstElementChild;
+const nav = document.querySelector('.nav'); 
+const navLink = document.querySelectorAll('.nav-link'); 
+
+//mouseenter event
+//hover effect on nav links
+navLink.forEach(link => {
+    link.addEventListener('mouseenter', () => {
+        link.style.backgroundColor = 'lightblue'
+    })
+})
+navLink.forEach(link => {
+    link.addEventListener('mouseout', () => {
+        link.style.backgroundColor = '#fff'
+    })
+})
+
+
+
+
+
