0ace8666dd77d48b3fe53caecc2e7475f6024d29
Katiuska <katiuskaalicea@gmail.com>
2020-07-12 16:58:04 -0700

    added 4 more events

--- js/index.js
+++ js/index.js
@@ -14,24 +14,82 @@ navLink.forEach(link => {
         link.style.backgroundColor = 'lightblue'
     })
 })
+
+//mouseleave event
 navLink.forEach(link => {
     link.addEventListener('mouseout', () => {
         link.style.backgroundColor = '#fff'
     })
 })
 
+
+
+const contentSection = document.querySelector('.content-section');
+const firstContent = contentSection.firstElementChild;
+const imgContent = firstContent.nextElementSibling; 
+
+imgContent.addEventListener('mouseenter', () => {
+    imgContent.style.transform = 'scale(1.25)';
+    imgContent.style.transition = 'all 2s';
+})
+
+imgContent.addEventListener('mouseleave', () => {
+    imgContent.style.transform = 'scale(1)';
+    imgContent.style.transition = 'all 2s';
+})
+
+
+//form
+const inputFields = document.querySelectorAll('input');
+inputFields.forEach(input => {
+    if(input.type === 'text') {
+        input.style.backgroundColor = "#fff";
+    }
+}); 
+
+
+//onfocus event
+inputFields.forEach(input => {
+    input.addEventListener('focus', () => {
+        if (input.type === 'text') {
+            input.style.backgroundColor = '#15A3B8'; 
+        }
+    })
+})
+
+//blur event
+inputFields.forEach(input => {
+    if (input.type === 'text') {
+        input.addEventListener('blur', () => {
+            input.style.backgroundColor = '#fff';
+        })
+    }
+})
+
+const button = document.querySelectorAll('.btn');
+
 //click event
-introImg.addEventListener('mouseenter', () => {
-    introImg.style.transform = 'scale(1.25)';
-    // introImg.style.opacity = '.4';
-    introImg.style.zIndex = '-100'
-    introImg.style.transition = 'all 2s';
+button.forEach(button => {
+    button.addEventListener('click', () => {
+        alert('Fill the form!');
+    })
 })
-introImg.addEventListener('mouseleave', () => {
-    introImg.style.transform = 'scale(1)';
-    introImg.style.opacity = '1';
-    introImg.style.transition = 'all 2s';
+
+//dblclick event
+h1.addEventListener('dblclick', () => {
+    h1.textContent = 'Hop On!'; 
 })
+
+const destination = document.querySelector('.destination');
+const h4 = document.querySelectorAll('h4');
+
+/* h4.forEach(title => 
+    title.addEventListener('select', () => {
+        title.style.backgroundColor = 'hotpink'
+    })
+) */
+
+const inverseContent = document.querySelector('.inverse-content');
 
 
 
